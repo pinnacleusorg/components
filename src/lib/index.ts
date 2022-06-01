604afc9a595a397f655cfd4ad4309e1fabc97615
SDBagel <sdbagel@gmail.com>
2022-06-01 12:58:11 -0700

    feat: Add default exports

---
+++ src/lib/index.ts
@@ -0,0 +1,12 @@
+export {default as Button} from "./Button.svelte";
+
+export {default as InputFile} from "./InputFile.svelte";
+export {default as InputText} from "./InputText.svelte";
+export {default as InputTextArea} from "./InputTextArea.svelte";
+export {default as InputTextLines} from "./InputTextLines.svelte";
+
+export {default as Option} from "./Option.svelte";
+export {default as OptionCheckbox} from "./OptionCheckbox.svelte";
+export {default as OptionGroup} from "./OptionGroup.svelte";
+export {default as OptionRadio} from "./OptionRadio.svelte";
+export {default as OptionText} from "./OptionText.svelte";

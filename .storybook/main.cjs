d6b716fd927ac7a2f4f4f71cce30e13a98cb0125
ivynya <ivy@ivy.direct>
2023-06-27 11:24:45 -0700

    repo: build/pub configuration change

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,31 +1,12 @@
-const preprocess = require('svelte-preprocess');
-const {
-  mergeConfig
-} = require('vite');
-
-// Base path gets inserted here with CI/CD
-const basePath = "";
 module.exports = {
-  async viteFinal(config, {
-    configType
-  }) {
-    // return the customized config
-    return mergeConfig(config, {
-      // customize the Vite config here
-      base: basePath
-    });
-  },
-  stories: [
-  // "../src/**/*.stories.mdx",
-  "../src/**/*.stories.@(js|jsx|ts|tsx|mdx)"],
-  addons: [{
-    name: "@storybook/addon-essentials"
-  }, "@storybook/addon-docs", "@storybook/addon-mdx-gfm"],
-  framework: {
-    name: "@storybook/sveltekit",
-    options: {}
-  },
-  docs: {
-    autodocs: true
-  }
+	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+	addons: ['@storybook/addon-essentials', '@storybook/addon-docs'],
+	framework: {
+		name: '@storybook/sveltekit',
+		options: {}
+	},
+	docs: {
+		autodocs: true,
+		docsMode: true
+	}
 };

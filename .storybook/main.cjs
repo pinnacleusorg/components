6f4a287a2a3e51e62f3666dc1b05a5e0a08faf82
ivynya <ivy@ivy.direct>
2023-07-24 22:48:07 -0700

    repo: upgrade storybook

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,12 +1,12 @@
 module.exports = {
-	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-	addons: ['@storybook/addon-essentials', '@storybook/addon-docs'],
-	framework: {
-		name: '@storybook/sveltekit',
-		options: {}
-	},
-	docs: {
-		autodocs: true,
-		docsMode: true
-	}
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-docs', "@storybook/addon-mdx-gfm"],
+  framework: {
+    name: '@storybook/sveltekit',
+    options: {}
+  },
+  docs: {
+    autodocs: true,
+    docsMode: true
+  }
 };

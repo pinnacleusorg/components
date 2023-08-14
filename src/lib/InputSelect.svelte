7c908b7256084e4e68e726d1221322f51fb05dac
ivynya <ivy@ivy.direct>
2023-08-13 21:44:26 -0700

    feat: two-way binding for select

--- src/lib/InputSelect.svelte
+++ src/lib/InputSelect.svelte
@@ -6,13 +6,15 @@
 	// Name in form data
 	export let name: string;
 	$: id = `${name}-${Math.round(Math.random() * 1e6)}`;
+	// Two-way binding for selected value
+	export let value: string;
 
 	export let active = false;
 </script>
 
 <div class="input">
 	<Label forId={id} {active}>{label}</Label>
-	<select {id} {name} required>
+	<select {id} {name} bind:value required>
 		<slot />
 	</select>
 </div>
